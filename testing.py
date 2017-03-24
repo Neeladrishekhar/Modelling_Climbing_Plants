a817040c69c90cbc445ce86b1eb7c38c67329433
Neeladrishekhar <Neeladrishekhar29@gmail.com>
2017-03-24 19:00:41 +0530

    basic python script for key frame animation

--- testing.py
+++ testing.py
@@ -1,39 +1,26 @@
-import bpy.ops
-# import bpy
+# import bpy.ops
+import bpy
+import math
 
-# bpy.ops.mesh.primitive_uv_sphere_add()
-# bpy.ops.transform.resize(value=(1,1,2))
-# bpy.ops.transform.translate(value=(0,0,2))
+ctx = bpy.context
+ops = bpy.ops
 
+ctx.scene.frame_start = 0
+ctx.scene.frame_end = 21
 
-# prepare a scene
-scn = bpy.context.scene
-scn.frame_start = 1
-scn.frame_end = 101
+ctx.scene.frame_current = 0
+ops.mesh.primitive_uv_sphere_add()
+ops.object.shade_smooth()
+ob = ctx.active_object
+ob.name = "Root0"
+ops.anim.keyframe_insert_menu(type='Location')
+ops.anim.keyframe_insert_menu(type='Scaling')
 
-# move to frame 17
-bpy.ops.anim.change_frame(frame = 17)
+ctx.scene.frame_current = 20
+ops.object.select_pattern(pattern="Root0")
+ob.location = (0,0,2)
+ob.scale = (1,1,2)
+ops.anim.keyframe_insert_menu(type='Location')
+ops.anim.keyframe_insert_menu(type='Scaling')
 
-# create an object
-bpy.ops.object.add(type='MESH')
-newObject = bpy.context.object
-newObject.name = "MyTriangle"
-newMesh = newObject.data
-x, y, z = 0, 0, 0
-width, depth, height = 1, 1, 0.5
-newVerts = [(x,y,z), (x+width,y,z), (x+width,y+depth,z), (x,y+depth,z),
-           (x,y,z+height), (x+width,y,z+height), (x+width,y+depth,z+height),
-           (x,y+depth,z+height)]
-newEdges = []       # creating vertices and faces is sufficient.
-newFaces = [(0,1,2,3), (2,3,7,6), (4,5,6,7), (0,3,7,4), (1,2,6,5), (0,1,5,4)]
-newMesh.from_pydata(newVerts, newEdges, newFaces)
-newMesh.update()
-
-# select the created object
-bpy.ops.object.select_name(name="myTriangle")
-
-# do something with the object. A rotation, in this case
-bpy.ops.transform.rotate(value=(-0.5*pi, ), axis=(-1, 0, 0))
-
-# create keyframe
-bpy.ops.anim.keyframe_insert_menu(type='Rotation')
+ctx.scene.frame_current = 0
